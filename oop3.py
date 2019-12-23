19b372ae6ac37939890832225a8c777a3b3b19fb
swatinyk <spawar1@flextrade.com>
2019-12-23 17:26:38 +0530

    ggg

---
+++ oop3.py
@@ -0,0 +1,22 @@
+class employee:
+    def __init__(self,eno,ename,esal):
+        self.eno=eno
+        self.ename=ename
+        self.esal=esal
+
+    def display(self):
+        print("employee no is : ",self.eno)
+        print("employee name is : ",self.ename)
+        print("employee salary is : ",self.esal)
+
+
+e=employee(10,'swati',2000)
+
+class test:
+    def modify(e):
+        e.esal=e.esal+1000
+        e.display()
+
+
+
+test.modify(e)
